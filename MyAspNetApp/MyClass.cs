07056c81bbfbaa75e62225060282d63dddb6b640
tsubakimoto <tkmbmbr@gmail.com>
2019-10-30 13:27:37 +0900

    add MyClass.cs

---
+++ MyAspNetApp/MyClass.cs
@@ -0,0 +1,12 @@
+﻿namespace MyAspNetApp
+{
+    public class MyClass
+    {
+        private MyAspNetApp.Models.MyAspNetAppContext context;
+
+        public void Greet(MyAspNetApp.Models.MyAspNetAppContext context)
+        {
+            this.context = context;
+        }
+    }
+}
